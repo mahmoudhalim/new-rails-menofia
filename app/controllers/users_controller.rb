f38f9f4dd5ab33c864472d20780ce0268fa9c791
mahmoudhalim <mahmoudahalim0@gmail.com>
2026-04-17 23:56:13 +0200

    feat: implement user CRUD actions and validations, update form to include address field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,55 @@
 class UsersController < ApplicationController
-  # your code goes here
+  before_action :set_user, only: %i[ show edit update destroy ]
+
+  # GET /users
+  def index
+    @users = User.all
+  end
+
+  # GET /users/:id
+  def show
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/:id/edit
+  def edit
+  end
+
+  # POST /users
+  def create
+    @user =  User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH /users/:id
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/:id
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # to create user from params directly
+  def user_params
+    params.require(:user).permit(:name, :dob, :email, :phone, :address)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  # validate
+  validates :name, presence: true
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :dob, presence: true
+  validates :phone, presence: true
 end
