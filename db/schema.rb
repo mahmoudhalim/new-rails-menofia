a8ceecfa720a75ca99f9235d1812d43fda61ca39
mahmoudhalim <mahmoudahalim0@gmail.com>
2026-04-17 23:55:34 +0200

    feat: add email and address columns to users table with unique index on email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_17_105419) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_17_201611) do
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
@@ -19,10 +19,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_17_105419) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "address", null: false
     t.datetime "created_at", null: false
     t.date "dob"
+    t.string "email"
     t.string "name"
     t.integer "phone"
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
