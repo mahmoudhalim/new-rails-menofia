94f46e80327045c3748f3f5b338c309f14e02415
mahmoudhalim <mahmoudahalim0@gmail.com>
2026-04-17 23:54:13 +0200

    feat: implement posts controller actions and validations for post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,50 @@
 class PostsController < ApplicationController
-  # your code goes here
+  before_action :set_post, only: %i[ show edit update destroy ]
+
+  # GET /posts
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/:id
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/:id/edit
+  def edit
+  end
+
+  # POST /posts/commit
+  def create
+    @post = Post.new(title: params[:post][:title], content: params[:post][:content])
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH /posts/:id
+  def update
+    if @post.update(title: params[:post][:title], content: params[:post][:content])
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/:id
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    # validation
+    validates :title, length: { minimum: 5 }
+    validates :content, presence: true, length: { maximum: 120 }
 end
